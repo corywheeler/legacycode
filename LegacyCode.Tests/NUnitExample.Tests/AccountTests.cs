2c56072d8868cb0d33da97baa02bbce38616f27f
Cory Wheeler <corywheeler@gmail.com>
2015-07-30 11:40:59 -0400

    tabify the file

--- LegacyCode.Tests/NUnitExample.Tests/AccountTests.cs
+++ LegacyCode.Tests/NUnitExample.Tests/AccountTests.cs
@@ -5,49 +5,49 @@ namespace Bank
 	[TestFixture]
   public class AccountTest
   {
-    Account source;
-    Account destination;
+	Account source;
+	Account destination;
 
-    [SetUp]
-    public void Init()
-    {
-      source = new Account();
-      source.Deposit(200m);
+	[SetUp]
+	public void Init()
+	{
+	  source = new Account();
+	  source.Deposit(200m);
 
-      destination = new Account();
-      destination.Deposit(150m);
-    }
+	  destination = new Account();
+	  destination.Deposit(150m);
+	}
 
-    [Test]
-    public void TransferFunds()
-    {
-      source.TransferFunds(destination, 100m);
+	[Test]
+	public void TransferFunds()
+	{
+	  source.TransferFunds(destination, 100m);
 
-      Assert.AreEqual(250m, destination.Balance);
-      Assert.AreEqual(100m, source.Balance);
-    }
+	  Assert.AreEqual(250m, destination.Balance);
+	  Assert.AreEqual(100m, source.Balance);
+	}
 
-    [Test]
-    [ExpectedException(typeof(InsufficientFundsException))]
-    public void TransferWithInsufficientFunds()
-    {
-      source.TransferFunds(destination, 300m);
-    }
+	[Test]
+	[ExpectedException(typeof(InsufficientFundsException))]
+	public void TransferWithInsufficientFunds()
+	{
+	  source.TransferFunds(destination, 300m);
+	}
 
-    [Test]
-    [Ignore("Decide how to implement transaction management")]
-    public void TransferWithInsufficientFundsAtomicity()
-    {
-      try
-      {
-        source.TransferFunds(destination, 300m);
-      }
-      catch (InsufficientFundsException expected)
-      {
-      }
+	[Test]
+	[Ignore("Decide how to implement transaction management")]
+	public void TransferWithInsufficientFundsAtomicity()
+	{
+	  try
+	  {
+		source.TransferFunds(destination, 300m);
+	  }
+	  catch (InsufficientFundsException expected)
+	  {
+	  }
 
-      Assert.AreEqual(200m, source.Balance);
-      Assert.AreEqual(150m, destination.Balance);
-    }
+	  Assert.AreEqual(200m, source.Balance);
+	  Assert.AreEqual(150m, destination.Balance);
+	}
   }
 }
