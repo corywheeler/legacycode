0646fd221c19775d1a080b0be92057008fa8b25a
Cory Wheeler <corywheeler@gmail.com>
2015-09-22 09:47:21 -0400

    remove unused usings

--- Chapter.8/AnonymousMessageForwarder.cs
+++ Chapter.8/AnonymousMessageForwarder.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chapter._8
+﻿namespace Chapter._8
 {
 	public class AnonymousMessageForwarder: MessageForwarder
 	{
