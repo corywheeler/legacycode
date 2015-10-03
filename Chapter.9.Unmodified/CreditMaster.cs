e7a37a5aa09e30cb904939b3d354206fd59f56ad
Cory Wheeler <corywheeler@gmail.com>
2015-10-03 09:00:57 -0400

    Initial creation of the CreditValidator, CreditMaster, and RGHConnection classes.

---
+++ Chapter.9.Unmodified/CreditMaster.cs
@@ -0,0 +1,14 @@
+﻿namespace Chapter._9.Unmodified
+{
+	public class CreditMaster
+	{
+		private readonly string _filename;
+		private readonly bool _isLocal;
+
+		public CreditMaster(string filename, bool isLocal)
+		{
+			_filename = filename;
+			_isLocal = isLocal;
+		}
+	}
+}
---
+++ Chapter.9.Unmodified/CreditValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace Chapter._9.Unmodified
+{
+	class CreditValidator
+	{
+		private readonly RGHConnection _connection;
+		private readonly CreditMaster _master;
+		private readonly string _validatorId;
+
+		public CreditValidator(RGHConnection connection, CreditMaster master, string validatorID)
+		{
+			_connection = connection;
+			_master = master;
+			_validatorId = validatorID;
+		}
+	}
+}
