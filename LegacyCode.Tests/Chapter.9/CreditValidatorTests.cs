d6690724cf8df45b96e6a3fbbfd9b0c21aa744c4
Cory Wheeler <corywheeler@gmail.com>
2015-11-05 11:12:21 -0500

    Test using FakeConnection instead of the RGHConnection class to avoid real connections being made to some remote system.

---
+++ LegacyCode.Tests/Chapter.9/CreditValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Chapter._9;
+using NUnit.Framework;
+
+namespace LegacyCode.Tests.Chapter._9
+{
+	[TestFixture]
+	class CreditValidatorTests
+	{
+		private Certificate _result;
+
+		[SetUp]
+		public void Init()
+		{
+			CreditMaster master = new CreditMaster("crm2.mas", true);
+			IRGHConnection connection = new FakeConnection();
+			CreditValidator validator = new CreditValidator(connection, master, "a");
+			connection.Report = new RFDIReport();
+
+			_result = validator.ValidateCustomer(new Customer());
+		}
+
+		[Test]
+		public void testNoSuccess()
+		{
+			Assert.AreEqual(Certificate.Valid, _result.GetStatus());
+
+		}
+
+	}
+}
---
+++ LegacyCode.Tests/Chapter.9/FakeConnection.cs
@@ -0,0 +1,32 @@
+﻿using Chapter._9;
+
+namespace LegacyCode.Tests.Chapter._9
+{
+	class FakeConnection : IRGHConnection
+	{
+
+
+		public RFDIReport Report { get; set; }
+
+		public void Connect()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void DisConnect()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public RFDIReport RFDIReportFor(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public ACTIOReport ACTIOReportFor(int customerID)
+		{
+			throw new System.NotImplementedException();
+		}
+
+	}
+}
