bcfd4370b99758f358e8128237f571902d6df3a1
Cory Wheeler <corywheeler@gmail.com>
2015-12-23 08:31:56 -0500

    Here you can actually see the the Onion Parameter that SchedulingTask is.
    Imagine that if I had pushed it further, giving both the Scheduler class
    and the MeetingResolver class dependencies on more objects, and then
    in turn those objects depend on other objects.
    
    If that had been the case, this line of newing up a SchedulingTaskPane
    would have been longer.
    
    I deliberately did not give Scheduler and MeetingResolver further dependencies
    in the inital setup as I wanted to get under way with showing the technique
    for breaking the depency of an Onion Parameter. Now I kind of wish I would
    have pushed it further as it would have further driven home the Onion Parameter
    case.

---
+++ LegacyCode.Tests/Chapter.9/SchedulingTaskPaneTests.cs
@@ -0,0 +1,15 @@
+﻿using Chapter._9;
+using NUnit.Framework;
+
+namespace LegacyCode.Tests.Chapter._9
+{
+	[TestFixture]
+	class SchedulingTaskPaneTests
+	{
+		[SetUp]
+		public void Init()
+		{
+			var schedulingTaskPane = new SchedulingTaskPane(new SchedulingTask(new Scheduler(), new MeetingResolver()));
+		}
+	}
+}
