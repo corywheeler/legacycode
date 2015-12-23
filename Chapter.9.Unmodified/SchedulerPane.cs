46c7d51b77b26eb1861b9251df6c823681771665
Cory Wheeler <corywheeler@gmail.com>
2015-12-23 07:57:34 -0500

    initial set of classes to demonstrate how to break the dependency of an Onion Parameter.

---
+++ Chapter.9.Unmodified/SchedulerPane.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter._9.Unmodified
+{
+	public class SchedulerPane
+	{
+		public SchedulerPane()
+		{
+			
+		}
+	}
+}
---
+++ Chapter.9.Unmodified/SerialTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chapter._9.Unmodified
+{
+	public class SerialTask
+	{
+		public SerialTask()
+		{
+			
+		}
+
+		public void Run()
+		{
+			Console.WriteLine("Run from SerialTask invoked.");
+		}
+
+		protected virtual void OverRidableMethod1()
+		{
+			Console.WriteLine("Called OverRidableMethod1 from SerialTask.");
+		}
+
+		protected virtual void OverRidableMethod2()
+		{
+			Console.WriteLine("Called OverRidableMethod2 from SerialTask.");
+		}
+
+	}
+}
