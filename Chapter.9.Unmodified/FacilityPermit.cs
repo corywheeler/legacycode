5938f4acde041ea78526b417cb1302f2aac4a5a1
Cory Wheeler <corywheeler@gmail.com>
2015-12-29 00:11:26 -0500

    define the hierarchy of Permit->FacilityPermit->OriginationPermit as well as the IndustrialFacility

---
+++ Chapter.9.Unmodified/FacilityPermit.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter._9.Unmodified
+{
+	public class FacilityPermit : Permit
+	{
+		public FacilityPermit(PermitNotice permitNotice) : base(permitNotice)
+		{
+			
+		}
+	}
+}
---
+++ Chapter.9.Unmodified/IndustrialFacility.cs
@@ -0,0 +1,27 @@
+﻿namespace Chapter._9.Unmodified
+{
+	public class IndustrialFacility
+	{
+		private Permit _basePermit;
+
+		public IndustrialFacility(int facilityCode, string owner, OriginationPermit permit)
+		{
+			Permit associatedPermit = PermitRepository.GetInstance().FindAssociatedFromOrigination(permit);
+
+			if (associatedPermit.IsValid() && !permit.IsValid())
+			{
+				_basePermit = associatedPermit;
+			} 
+			else if (!permit.IsValid())
+			{
+				permit.Validate();
+				_basePermit = permit;
+			}
+			else
+			{
+				throw new PermitViolation(permit);
+			}
+
+		}
+	}
+}
