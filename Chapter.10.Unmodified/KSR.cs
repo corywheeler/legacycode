02c7922e379f1149f9ecc4ec964c6a0b05f439aa
Cory Wheeler <corywheeler@gmail.com>
2017-05-31 21:24:47 -0400

    The Case of the "Helpful" Language Feature: Initial Commit - files needed to go through this example

---
+++ Chapter.10.Unmodified/KSR.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Web;
+
+namespace Chapter.Unmodified
+{
+    public class KSR
+    {
+        private const int MIN_LEN = 0;
+
+        public IList GetKSRStreams(HttpFileCollection files) 
+        {
+            ArrayList list = new ArrayList();
+
+            foreach (string name in files) 
+            {
+                HttpPostedFile file = files[name];
+
+                if (file.FileName.EndsWith(".ksr", System.StringComparison.CurrentCulture) ||
+                    (file.FileName.EndsWith(".txt", System.StringComparison.CurrentCulture) && file.ContentLength > MIN_LEN))
+                {
+                    list.Add(file.InputStream);
+                }
+            }
+
+            return list;
+        }
+    }
+}
